6bd900653cf293911da191e0eeb1deec46009f6c
William Yao <williamyaoh@gmail.com>
2017-01-08 22:53:12 -0600

    some quick util defs

--- colander.lisp
+++ colander.lisp
@@ -16,3 +16,54 @@
   #+gcl       si:*command-args*
   #+lispworks system:*line-arguments-list*
   #+sbcl      sb-ext:*posix-argv*)
+
+(defparameter *test-spec*
+  '("new" "foo" (:arg :name) (:arg "--loop")))
+
+(setf (symbol-function 'alpha-number-p) #'alphanumericp)
+
+(defun long-opt-p (str)
+  (and (stringp str)
+       (> (length str) 2)
+       (string= "--" str :end2 2)
+       (every #'alpha-number-p (subseq str 2))
+       str))
+
+(defun short-opt-p (str)
+  (and (stringp str)
+       (= (length str) 2)
+       (char= (char str 0) #\-)
+       (alpha-number-p (char str 1))
+       str))
+
+(defun combined-short-opt-p (str)
+  (and (stringp str)
+       (> (length str) 2)
+       (char= (char str 0) #\-)
+       (every #'alpha-number-p (subseq str 1))
+       str))
+
+(defun one-of (fn &rest fns)
+  (lambda (x)
+    (and (some (lambda (f) (funcall f x))
+               (cons fn fns))
+         x)))
+
+(defun all-of (fn &rest fns)
+  (lambda (x)
+    (and (every (lambda (f) (funcall f x))
+                (cons fn fns))
+         x)))
+
+(defun included-arg (str)
+  "Extract the included argument in the option; return them if found, NIL
+   if not."
+  (when (stringp str)
+    (let ((equal (position #\= str)))
+      (when equal
+        (if (funcall (one-of #'long-opt-p #'short-opt-p)
+                     (subseq str 0 equal))
+            (values (subseq str (1+ equal))
+                    (subseq str 0 equal))
+            (values nil nil))))))
+
