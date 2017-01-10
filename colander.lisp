12d4f598b1d73b97161a8f9abe910a49c734ba54
William Yao <williamyaoh@gmail.com>
2017-01-10 00:19:04 -0600

    argv formatting

--- colander.lisp
+++ colander.lisp
@@ -6,16 +6,16 @@
 
 (defun argv ()
   "Returns list of command-line arguments, including the name of executable."
-  #+abcl      ext:*command-line-argument-list*
-  #+allegro   sys:command-line-arguments
-  #+ccl       ccl:*command-line-argument-list*
-  #+clisp     (cons *load-truename* ext:*args*)
-  #+clozure   ccl::command-line-arguments
-  #+cmu       extensions:*command-line-words*
-  #+ecl       (ext:command-args)
-  #+gcl       si:*command-args*
+  #+abcl ext:*command-line-argument-list*
+  #+allegro sys:command-line-arguments
+  #+ccl ccl:*command-line-argument-list*
+  #+clisp (cons *load-truename* ext:*args*)
+  #+clozure ccl::command-line-arguments
+  #+cmu extensions:*command-line-words*
+  #+ecl (ext:command-args)
+  #+gcl si:*command-args*
   #+lispworks system:*line-arguments-list*
-  #+sbcl      sb-ext:*posix-argv*)
+  #+sbcl sb-ext:*posix-argv*)
 
 (defparameter *test-spec*
   '("new" "foo" :name (:arg "--loop" "LOOPAMT")
